8715ffee1cfd91a6e79d019bbad7f568fd25790d
Zalewa <zalewapl@gmail.com>
2014-12-31 00:43:03 +0100

    New project commit.

---
+++ android/src/masterofgalaxy/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package masterofgalaxy.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import masterofgalaxy.MogGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MogGame(), config);
+	}
+}
