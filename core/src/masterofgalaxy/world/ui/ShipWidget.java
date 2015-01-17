26581b792ad47dc0458bdfde22e9ca354c479b5a
Zalewa <zalewapl@gmail.com>
2015-01-17 20:59:02 +0100

    Ship amount selection slider.

--- core/src/masterofgalaxy/world/ui/ShipWidget.java
+++ core/src/masterofgalaxy/world/ui/ShipWidget.java
@@ -1,13 +1,19 @@
 package masterofgalaxy.world.ui;
 
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import masterofgalaxy.ecs.components.FleetComponent;
 
 public class ShipWidget extends Table {
     private Label nameLabel;
     private Label countLabel;
+    private Slider amountSlider;
+    private Label selectedAmountLabel;
     private Skin skin;
 
     public ShipWidget(Skin skin) {
@@ -22,10 +28,30 @@ public class ShipWidget extends Table {
 
         countLabel = new Label("", skin);
         add(countLabel).expandX().right();
+        row();
+
+        amountSlider = new Slider(0.0f, 0.0f, 1.0f, false, skin);
+        amountSlider.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                selectedAmountLabel.setText("" + getSelectedAmount());
+            }
+        });
+        add(amountSlider).expandX().center();
+
+        selectedAmountLabel = new Label("", skin);
+        add(selectedAmountLabel).right();
     }
 
     public void setShip(FleetComponent.Ship ship) {
         nameLabel.setText(ship.design.getName());
         countLabel.setText("" + ship.count);
+
+        amountSlider.setRange(0.0f, (float) ship.count);
+        amountSlider.setValue((float) ship.count);
+    }
+
+    public int getSelectedAmount() {
+        return MathUtils.round(amountSlider.getValue());
     }
 }
