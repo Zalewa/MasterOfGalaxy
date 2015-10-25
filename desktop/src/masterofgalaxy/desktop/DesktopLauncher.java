630f4a621db8be6aa909b94e30832b5115e22786
Zalewa <zalewapl@gmail.com>
2015-10-25 13:20:28 +0100

    uiskin atlas generated from separate picture files.

--- desktop/src/masterofgalaxy/desktop/DesktopLauncher.java
+++ desktop/src/masterofgalaxy/desktop/DesktopLauncher.java
@@ -1,11 +1,15 @@
 package masterofgalaxy.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import masterofgalaxy.MogGame;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
+		TexturePacker.processIfModified("ui/skin", "ui/", "uiskin");
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MogGame(), config);
 	}
